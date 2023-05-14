6efd05d3c5fbb9e2c6d8893955cf1a44e00ada1a
DaiCae <zyx0616666@outlook.com>
2023-05-14 20:41:43 +0800

    feat: update history service

--- src/app/module/history/__init__.py
+++ src/app/module/history/__init__.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, request, render_template
-from sqlalchemy import desc
 
 from app.entity.history import History
 
 bp = Blueprint('history', __name__, url_prefix='/history')
 
 
+@bp.route('/latest')
+def history_latest():
+    history_list = History.query.order_by(History.create_time.desc()).limit(100).all()
+    return render_template('history.html', title="Last 100 Records", history_list=history_list)
+
+
 @bp.route('/')
-def history_top100():
+def history_page():
     # 分页显示
     page = request.args.get('page', 1, type=int)
-    history_list = History.query.order_by(desc(History.create_time)).limit(100).all()
-    return render_template('history.html', history_list=history_list)
+    page = 1 if page <= 0 else page
+    history_list = History.query.order_by(History.create_time.desc()).paginate(page=page, per_page=100)
+    return render_template('history.html', title="Record List", history_list=history_list)
+
