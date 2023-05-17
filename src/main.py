0b08eceb0fe54e961bfc2d6d7ba76966182473d6
DaiCae <zyx0616666@outlook.com>
2023-05-17 10:39:03 +0800

    feat: add gevent

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from app import create_app
+from gevent import monkey
+
+monkey.patch_all()
 
 app = create_app()
 
 # gunicorn run
 bind = "[::]:5000"
 workers = 2
-# worker_class = "sync"
+worker_class = "gevent"
 
 # debug run
 if __name__ == '__main__':
