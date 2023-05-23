d8fea74207eb3346b123fdc9722882ded61f8035
DaiCae <zyx0616666@outlook.com>
2023-05-23 15:27:54 +0800

    refactor: adjust blueprint

--- src/app/extensions.py
+++ src/app/extensions.py
@@ -4,24 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 def init(app):
-    load_blueprint(app)
+    init_blueprint(app)
     db.init_app(app)
 
 
 db = SQLAlchemy()
 
 
-# 自动扫描加载蓝图
-def load_blueprint(app):
+# 扫描并注册蓝图
+def init_blueprint(app):
     path = ['app', 'blueprints']
-    # app/blueprints/
-    for file in os.listdir('/'.join(path)):
-        # app.blueprints.xxx
-        module_path = f'{path[0]}.{path[1]}.{file}'
-        if file.startswith("__"):
+    dir_path = '/'.join(path)  # app/blueprints/xxx
+    mod_path = '.'.join(path)  # app.blueprints.xxx
+    for file in os.listdir(dir_path):
+        if file.startswith('__'):
             continue
-        if file.endswith(".py"):
-            module_path = module_path[:-3]
-        module = importlib.import_module(module_path)
-        app.register_blueprint(getattr(module, "bp"))
-        print('Register blueprint: ' + module.__name__)
+        if file.endswith('.py'):
+            file = file[:-3]
+        module = importlib.import_module(f'{mod_path}.{file}')
+        app.register_blueprint(getattr(module, 'bp'))
+        print(f'Register blueprint: {module.__name__}')
