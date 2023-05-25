42bbd5c92cda8715c52ffb084d19167cfe45bf05
DaiCae <zyx0616666@outlook.com>
2023-05-26 00:27:10 +0800

    refactor: adjust blueprint

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,10 @@
+import importlib
+import os
+
 import yaml
 from flask import Flask
 
-from app import extensions
+from app.extensions import register_extensions
 
 config = yaml.load(open('config.yml', 'r'), Loader=yaml.FullLoader)
 
@@ -9,5 +12,19 @@ config = yaml.load(open('config.yml', 'r'), Loader=yaml.FullLoader)
 def create_app():
     app = Flask(__name__)
     app.config.update(config)
-    extensions.init(app)
+
+    register_blueprints(app)
+    register_extensions(app)
     return app
+
+
+def register_blueprints(app):
+    dir_path = f'{app.name}/blueprints'  # app/blueprints/xxx
+    mod_path = f'{app.name}.blueprints'  # app.blueprints.xxx
+    for file in os.listdir(dir_path):
+        if file.startswith('__'):
+            continue
+        mod_name = file[:-3] if file.endswith('.py') else file
+        module = importlib.import_module(f'{mod_path}.{mod_name}')
+        app.register_blueprint(getattr(module, 'bp'))
+        print(f'Register blueprint: {module.__name__}')
--- src/app/extensions.py
+++ src/app/extensions.py
@@ -1,26 +1,8 @@
-import os
-import importlib
 from flask_sqlalchemy import SQLAlchemy
 
 
-def init(app):
-    init_blueprint(app)
+def register_extensions(app):
     db.init_app(app)
 
 
 db = SQLAlchemy()
-
-
-# 扫描并注册蓝图
-def init_blueprint(app):
-    path = ['app', 'blueprints']
-    dir_path = '/'.join(path)  # app/blueprints/xxx
-    mod_path = '.'.join(path)  # app.blueprints.xxx
-    for file in os.listdir(dir_path):
-        if file.startswith('__'):
-            continue
-        if file.endswith('.py'):
-            file = file[:-3]
-        module = importlib.import_module(f'{mod_path}.{file}')
-        app.register_blueprint(getattr(module, 'bp'))
-        print(f'Register blueprint: {module.__name__}')
