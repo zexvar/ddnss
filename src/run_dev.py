03b20df4d026a7fb74d09cc657f0dccd4e5f4a74
DaiCae <zyx0616666@outlook.com>
2023-06-06 21:59:35 +0800

    feat: add run dev

---
+++ src/run_dev.py
@@ -0,0 +1,9 @@
+import yaml
+
+from app import create_app
+
+config = yaml.full_load(open('config-dev.yml'))
+app = create_app(config)
+
+if __name__ == '__main__':
+    app.run(host='::', debug=True)
