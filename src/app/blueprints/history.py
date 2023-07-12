a7dc0d48e941d4b5b17ebf945e283a90f1c494e0
DaiCae <zyx0616666@outlook.com>
2023-07-12 10:49:27 +0800

    feat: change history page

--- src/app/blueprints/history.py
+++ src/app/blueprints/history.py
@@ -8,19 +8,13 @@ bp = Blueprint("history", __name__)
 @bp.route("/latest")
 def history_latest():
     history_list = History.query.order_by(History.create_time.desc()).limit(100).all()
-    return render_template(
-        "history.html", title="Last 100 Records", history_list=history_list
-    )
+    return render_template("history.html", title="Latest records", history_list=history_list)
 
 
 @bp.route("/")
 def history_page():
     # 分页显示
-    page = request.args.get("page", 1, type=int)
+    page = request.values.get("page", 1, type=int)
     page = 1 if page <= 0 else page
-    history_list = History.query.order_by(History.create_time.desc()).paginate(
-        page=page, per_page=100
-    )
-    return render_template(
-        "history.html", title="Record List", history_list=history_list
-    )
+    history_list = History.query.order_by(History.create_time.desc()).paginate(page=page, per_page=100)
+    return render_template("history.html", title=f"Paginated view", history_list=history_list)
