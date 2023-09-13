3f38065821b09739e1d1a46ab23f945d9616d129
DaiCae <zyx0616666@outlook.com>
2023-09-13 08:21:14 +0800

    perf: reduce image size

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:slim-bullseye
+FROM python:alpine
 WORKDIR /ddnss
 ADD src .
 RUN python3 -m pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
--- src/init.sh
+++ src/init.sh
@@ -1,10 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 
-# gunicorn main:app \
-#   --workers=2 \
-#   --worker-class=gevent \
-#   --worker-connections=2000 \
-#   --bind [::]:5000
-
-gunicorn -w 2 -k gevent -b [::]:5000 main:app
+# gunicorn main:app --workers=2 --worker-class=gevent --bind [::]:5000
+gunicorn main:app -w 2 -k gevent -b [::]:5000
