003cf2844a8cb25df06f488d8963142aa6cfa80a
DaiCae <zyx0616666@outlook.com>
2023-09-13 08:18:33 +0800

    perf: register blueprint

--- src/app/core/blueprint.py
+++ src/app/core/blueprint.py
@@ -1,11 +1,22 @@
-import importlib
 import os
+from importlib import import_module
+
+from flask import Blueprint
 
 
 def register_blueprints(app):
-    dot_path = ".".join(paths := [app.name, "blueprints"])
-    for file in list(filter(lambda f: not str(f).startswith("__"), os.listdir(os.path.join(*paths)))):
-        bp = getattr(importlib.import_module(dot_path + "." + file.removesuffix(".py")), "bp")
-        bp.url_prefix = ("/" + bp.name) if bp.url_prefix is None else bp.url_prefix
+    basepath = f"{app.name}.blueprints"
+    for file in os.listdir(basepath.replace(".", "/")):
+        if not file.startswith("__"):
+            module = import_module(f"{basepath}.{file}".removesuffix(".py"))
+            register_module_bp(module, app)
+
+
+def register_module_bp(module, app):
+    bp = getattr(module, "bp", None)
+    if isinstance(bp, Blueprint):
+        if bp.url_prefix is None:
+            bp.url_prefix = f"/{bp.name}"
         app.register_blueprint(bp)
         print(f"Register blueprint: {bp.import_name} {bp.url_prefix}")
+    return None
