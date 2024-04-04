2726d3c77ef28b3cd3e6238a4c9fef72cd122ce3
DaiCae <zyx0616666@outlook.com>
2024-04-05 00:13:51 +0800

    feat: update templates and history bp

--- app/blueprints/history.py
+++ app/blueprints/history.py
@@ -1,21 +1,40 @@
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 
 from app.blueprints import Blueprint
-from app.models import History
+from app.models import History, PaginatedQuery
 
 bp = Blueprint("history", __name__, url_prefix="/history")
 
 
-@bp.route("/latest")
-def history_latest():
-    history_list = History.select().order_by(History.create_time.desc()).limit(100)
-    return render_template("history.jinja", title="Latest records", history_list=history_list)
-
-
 @bp.route("/")
-def history_page():
+def index():
+    return redirect(url_for("history.page_view", page=1))
+
+
+@bp.route("/view")
+def page_view():
     # 分页显示
-    page = request.values.get("page", 1, type=int)
-    page = 1 if page <= 0 else page
-    history_list = History.select().order_by(History.create_time.desc()).paginate(page, 100)
-    return render_template("history.jinja", title=f"Paginated view", history_list=history_list)
+    page = max(request.values.get("page", 1, type=int), 1)
+    page_size = 20
+
+    pagination = PaginatedQuery(
+        History.select().order_by(History.create_time.desc()),
+        paginate_by=page_size,
+        page=page,
+    )
+
+    page_max = pagination.get_page_count()
+    page_prev = page - 1 if page - 1 >= 1 else False
+    page_next = page + 1 if page + 1 <= page_max else False
+    page_range = pagination.get_page_range(page, page_max, show=11)
+    object_list = pagination.get_object_list()
+
+    return render_template(
+        "history.jinja",
+        page=page,
+        page_max=page_max,
+        page_prev=page_prev,
+        page_next=page_next,
+        page_range=page_range,
+        object_list=object_list,
+    )
