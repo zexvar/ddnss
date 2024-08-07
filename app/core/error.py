eddad65b05adfb71f026311947968f355e0e608a
DaiCae <zyx0616666@outlook.com>
2024-08-07 22:51:31 +0800

    refactor: change core.error

--- app/core/error.py
+++ app/core/error.py
@@ -1,19 +1,26 @@
-from app.utils import response
+from .resp import Html, Rest, response
+
+
+def error_response(e, template):
+    return response(
+        Rest.error(repr(e)),
+        Html.render(template),
+    )
 
 
 def register_error_handler(app):
     @app.errorhandler(400)
     def error_400(e: Exception):
-        return response.resp("errors/400.jinja", e)
+        return error_response(e, "errors/400.jinja")
 
     @app.errorhandler(404)
     def error_404(e: Exception):
-        return response.resp("errors/404.jinja", str(e))
+        return error_response(e, "errors/404.jinja")
 
     @app.errorhandler(500)
     def error_500(e: Exception):
-        return response.resp("errors/500.jinja", str(e))
+        return error_response(e, "errors/500.jinja")
 
     @app.errorhandler(Exception)
     def error_default(e: Exception):
-        return response.resp("errors/404.jinja", str(e))
+        return error_response(e, "errors/500.jinja")
