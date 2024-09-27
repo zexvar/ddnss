ef9e85742c474ea563aa95976f6a49cebadc2a7e
DaiCae <zyx0616666@outlook.com>
2024-09-27 22:53:43 +0800

    perf: blueprint register logic

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -1,3 +1,4 @@
+from collections import deque
 from importlib import import_module
 from pathlib import Path
 
@@ -14,15 +15,22 @@ class Blueprint(FlaskBlueprint):
         return instance
 
 
-def register_blueprints(app: Flask, url_prefix=None):
-    # Scan modules
-    for file in Path(__file__).parent.iterdir():
-        if file.is_dir() or file.suffix == ".py":
-            import_module(f"{__name__}.{file.stem}")
+def register_blueprints(app: Flask, root_path=Path(__file__).parent):
+    # Scan modules -- BFS
+    queue = deque([root_path.relative_to(Path.cwd())])
+    while queue:
+        current = queue.popleft()
+        for file in current.glob("*"):
+            if file.name.startswith("__"):
+                continue
+            elif (file.is_file() and file.suffix == ".py") or (file.is_dir() and (file / "__init__.py").exists()):
+                module = ".".join(file.with_suffix("").parts)
+                print(f"Import module: {module}")
+                import_module(module)
+            elif file.is_dir():
+                queue.append(file)
 
     # Register `flask.Blueprint` on the app
     for bp in Blueprint.registry:
-        if url_prefix:
-            bp.url_prefix = f"/{bp.url_prefix}/{url_prefix}"
         print(f"Register blueprint: {bp.import_name} {bp.url_prefix}")
         app.register_blueprint(bp)
