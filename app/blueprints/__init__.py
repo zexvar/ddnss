9d2deed39642caa07bc0eefce4a401465f5d040e
DaiCae <zyx0616666@outlook.com>
2024-10-14 10:35:40 +0800

    perf: blueprint register logic

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -1,5 +1,5 @@
+import importlib
 from collections import deque
-from importlib import import_module
 from pathlib import Path
 
 from flask import Blueprint as FlaskBlueprint
@@ -15,18 +15,19 @@ class Blueprint(FlaskBlueprint):
         return instance
 
 
-def register_blueprints(app: Flask, root_path=Path(__file__).parent):
+def register_blueprints(app: Flask, scan_path=None):
+    scan_path = Path(scan_path or f"{app.name}/blueprints")
     # Scan modules -- BFS
-    queue = deque([root_path.relative_to(Path.cwd())])
+    queue = deque([scan_path])
     while queue:
-        current = queue.popleft()
-        for file in current.glob("*"):
+        for file in queue.popleft().glob("*"):
             if file.name.startswith("__"):
                 continue
-            elif (file.is_file() and file.suffix == ".py") or (file.is_dir() and (file / "__init__.py").exists()):
+            # Import file module or package module
+            elif file.is_file() and file.suffix == ".py" or Path(file / "__init__.py").exists():
                 module = ".".join(file.with_suffix("").parts)
                 print(f"Import module: {module}")
-                import_module(module)
+                importlib.import_module(module)
             elif file.is_dir():
                 queue.append(file)
 
