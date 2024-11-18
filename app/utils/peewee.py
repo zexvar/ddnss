6ce1c5d7134dc55333d58c60ace858d5e68f1c80
zero <zyx0616666@outlook.com>
2024-11-18 22:34:13 +0800

    perf: change peewee init logic

--- app/utils/peewee.py
+++ app/utils/peewee.py
@@ -1,14 +1,74 @@
-from playhouse.flask_utils import FlaskDB
+from flask import Flask
+from peewee import Database, Model, Proxy
+from playhouse.db_url import connect
 from playhouse.flask_utils import PaginatedQuery as PeeweePaginatedQuery
 
 
-def subclasses(cls):
-    return set(cls.__subclasses__()).union([s for c in cls.__subclasses__() for s in subclasses(c)])
+def subclasses(clazz):
+    dedupe = set()
+    subclasses_list = []
+
+    def collect_subclasses(cls):
+        for subclass in cls.__subclasses__():
+            if subclass not in dedupe:
+                dedupe.add(subclass)
+                subclasses_list.append(subclass)
+                collect_subclasses(subclass)
+
+    collect_subclasses(clazz)
+    return subclasses_list
 
 
-class PeeweeORM(FlaskDB):
-    def create_all(self):
-        self.database.create_tables(subclasses(self.Model))
+class PeeweeORM:
+    def __init__(self, app=None, database=None, connect_url=None):
+        self.database = Proxy()
+        self._db = database
+        self._db_url: str = connect_url
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask):
+        self.app = app
+        if not self._db:
+            self._db_url = self._db_url or app.config.get("DATABASE") or app.config["DATABASE_URL"]
+            if not self._db_url:
+                raise ValueError("Missing required configuration data for " "database: DATABASE or DATABASE_URL.")
+            self._db = connect(self._db_url)
+
+        if isinstance(self._db, Database):
+            self.database.initialize(self._db)
+        elif isinstance(self._db, Proxy):
+            self.database = self._db
+        else:
+            raise RuntimeError("Database initialize failed.")
+
+        app.before_request(self.connect_db)
+        app.teardown_request(self.close_db)
+
+    def connect_db(self):
+        self.database.connect()
+
+    def close_db(self, exc):
+        if not self.database.is_closed():
+            self.database.close()
+
+    def create_all(self, ignore_base_model=True):
+        models = [
+            model
+            for model in subclasses(Model)
+            if getattr(model, "_meta").database == self.database
+            and (not ignore_base_model or not model.__name__.startswith("Base"))
+        ]
+        print(f"Database create tables: {models}")
+        self.database.create_tables(models)
+
+    @property
+    def Model(self):
+        class BaseModel(Model):
+            class Meta:
+                database = self.database
+
+        return BaseModel
 
 
 class PaginatedQuery(PeeweePaginatedQuery):
