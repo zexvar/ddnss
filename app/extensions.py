560f4f0e877004284bbe0cb0c2b778cb054314ea
zero <zyx0616666@outlook.com>
2025-07-05 03:32:50 +0800

    fix: can not auto create tables

--- app/extensions.py
+++ app/extensions.py
@@ -6,6 +6,9 @@ orm = PeeweeORM()
 def init_db(app):
     orm.init_app(app)
     with app.app_context():
+        # Ensure models are imported to register them
+        from app import models
+
         orm.create_all()
 
 
