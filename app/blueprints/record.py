ce7b6d153943f5b2c44df3811f3aaedc44d69a2c
zero <zyx0616666@outlook.com>
2025-11-21 11:42:31 +0800

    feat: add record page

--- app/blueprints/record.py
+++ app/blueprints/record.py
@@ -2,10 +2,10 @@ from flask import Blueprint, request
 
 from app.core.auth import Auth
 from app.core.resp import Html
-from app.models import History
+from app.models import Record
 from app.utils import peewee
 
-bp = Blueprint("history", __name__, url_prefix="/history")
+bp = Blueprint("record", __name__, url_prefix="/record")
 
 
 @bp.route("/")
@@ -13,19 +13,19 @@ bp = Blueprint("history", __name__, url_prefix="/history")
 def index():
     page = request.values.get("page", 1, type=int)
     limit = request.values.get("limit", 10, type=int)
-    query = History.select(
-        History.id,
-        History.name,
-        History.type,
-        History.content,
-        History.success,
-        History.create_time,
-    ).order_by(History.create_time.desc())
+    query = Record.select(
+        Record.id,
+        Record.name,
+        Record.type,
+        Record.content,
+        Record.create_time,
+    ).order_by(Record.create_time.desc())
 
     result = peewee.OffsetPagination(query, page, limit)
+
     return Html.render(
-        "history.jinja",
-        url="/history",
+        "record.jinja",
+        url="/record",
         items=result.items,
         pagination=result.pagination,
         status=200,
