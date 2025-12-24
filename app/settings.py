82940d09c3bf57e286fd1162ea8dfb309e7dfe99
zexvar <zyx0616666@outlook>
2025-12-24 23:25:45 +0800

    refactor: restructure config and loading logic

--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,12 @@
 import os
 import tomllib
-from dataclasses import dataclass
 
 # basedir for instance_path
 BASEDIR = os.path.abspath("data")
 TOML_FILE = os.path.join(BASEDIR, "config.toml")
 
 
-@dataclass
-class Config:
+class CONFIG:
     DEBUG: bool = False
 
     AUTH_TOKEN: str = None
@@ -20,31 +18,42 @@ class Config:
     SECRET_KEY: str = os.urandom(32).hex()
     DATABASE_URL: str = f"sqlite:///{BASEDIR}/data.db"
 
-    @staticmethod
-    def convert_type(value: str, type):
-        """Convert environment variable to the target type"""
-        if type is bool:
-            return value.lower() in ("true", "1", "yes")
-        if type is int:
-            return int(value)
-        if type is float:
-            return float(value)
-        return value
+    @classmethod
+    def load(cls):
+        cls.load_toml()
+        cls.load_env()
+        return cls
 
-    def __post_init__(self):
-        """Override values with environment variables"""
-        for key in self.__annotations__.keys():
-            value = os.getenv(key.upper())
-            if value is not None:
-                type = self.__annotations__[key]
-                setattr(self, key, self.convert_type(value, type))
+    @classmethod
+    def load_toml(cls):
+        if not os.path.exists(TOML_FILE):
+            return
 
-
-def load_config() -> Config:
-    if os.path.exists(TOML_FILE):
         with open(TOML_FILE, "rb") as f:
-            return Config(**tomllib.load(f))
-    return Config()
+            data = tomllib.load(f)
+
+        for key, value in data.items():
+            if hasattr(cls, key):
+                setattr(cls, key, value)
+
+    @classmethod
+    def load_env(cls):
+        def convert_type(value: str, type):
+            """Convert environment variable to the target type"""
+            if type is bool:
+                return value.lower() in ("true", "1", "yes")
+            if type is int:
+                return int(value)
+            if type is float:
+                return float(value)
+            return value
+
+        for key in dir(cls):
+            if key.isupper() and key in os.environ:
+                raw = os.environ[key]
+                default = getattr(cls, key)
+                cls_value = convert_type(raw, type(default))
+                setattr(cls, key, cls_value)
 
 
-CONFIG = load_config()
+CONFIG.load()
